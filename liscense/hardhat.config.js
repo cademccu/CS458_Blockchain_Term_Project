a9a13760f67a40863dc29fbf4fae354f662dd44b
Yosief G <yosief10@outlook.com>
2021-04-20 16:56:22 -0600

    Added basic user creation

--- liscense/hardhat.config.js
+++ liscense/hardhat.config.js
@@ -1,6 +1,12 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@nomiclabs/hardhat-waffle");
 module.exports = {
+  paths: {
+    sources: "./contracts",
+    artifacts: "./build",
+  },
   solidity: "0.7.3",
 };
+
